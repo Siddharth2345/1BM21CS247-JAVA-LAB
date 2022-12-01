464455cfce349c9bb4e4fef658df337cd56536a5
Siddharth2345 <117808877+Siddharth2345@users.noreply.github.com>
2022-12-01 15:57:20 +0530

    Add files via upload

---
+++ bookDetails.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class book{
+String name;
+String author;
+int price;
+int num_pages;
+book(){
+name="RAM";
+author=" SHYAM";
+price=10;
+num_pages=5;
+}
+void set(){
+System.out.println("ENTER THE NAME OF THE BOOK");
+Scanner sc=new Scanner(System.in);
+name=sc.nextLine();
+System.out.println("ENTER THE AUTHOR");
+author=sc.nextLine();
+System.out.println("ENTER THE PRICE");
+price=sc.nextInt();
+System.out.println("ENTER THE NUMBER OF PAGES");
+num_pages=sc.nextInt();
+}
+public String toString(){
+return("NAME OF THE BOOK : "+name+"\n"+"AUTHOR : "+author+"\n"+"PRICE : "+price+"\n"+"NUMBER OF PAGES"+num_pages);
+}
+}
+class bookDetails{
+public static void main(String args[]){
+int n;
+System.out.println("ENTER THE NUMBER OF BOOKS");
+Scanner vb=new Scanner(System.in);
+n=vb.nextInt();
+book b1[]=new book[n];
+for(int i=0;i<n;i++){
+b1[i]=new book();
+b1[i].set();
+}
+for(int i=0;i<n;i++){
+System.out.println("DETAILS OF BOOK "+(i+1));
+System.out.println(b1[i]);
+}
+}
+}
+
+
